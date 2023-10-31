c56bf5414796b4c39420f4342777b9b27d5084cb
yingchi1130 <kiki001130@gmail.com>
2023-10-31 11:23:14 +0800

    week08

---
+++ week08/week08-6.py
@@ -0,0 +1,18 @@
+lass Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        d = {}
+        for c in s:
+            if c in d:
+                d[c] = d[c] + 1
+            else:
+                d[c] = 1
+        print(d) # 可以印看看,字母出現在次數統計結果
+
+        for c in t:
+            if c not in d: #不再字典裡,就是缺它,它就是多出來的
+                return c
+            if d[c]<=0: #不夠
+                return c #找出多出來的字母
+            else:
+                d[c] =d[c] - 1
+        return ""
