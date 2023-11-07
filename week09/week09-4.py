793cec884a007ee99c9a88334f1c150109a06d72
yingchi1130 <kiki001130@gmail.com>
2023-11-07 11:27:40 +0800

    week09

---
+++ week09/week09-4.py
@@ -0,0 +1,12 @@
+class Solution:
+    def canMakeArithmeticProgression(self, arr: List[int]) -> bool:
+        #現在還沒寫完，快完成了!
+
+        a = arr
+        N = len(a)
+        for i in range(N-1):
+            d = a[1] - a[0] #基礎的距離
+            if a[i+1] - a[i] != d: #距離不相等
+                return False
+        
+        return True
