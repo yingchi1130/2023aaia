55d244c5bb5001c6501b9c9b8baea006537ae529
yingchi1130 <kiki001130@gmail.com>
2023-11-21 11:08:46 +0800

    week11

---
+++ week11/week11-3a.py
@@ -0,0 +1,4 @@
+# week11-3a.py SOIT106_BASE_003
+a = list(map(int, input().split() ))
+
+print( sum(a) - a[0] )
---
+++ week11/week11-3b.py
@@ -0,0 +1,4 @@
+# week 11-3b.py SOIT106_BASE_006
+a = list(map(int,input() .split() ))
+
+print( max(a) )
