4fbe74c9d48a9a49e4dab9cfd81961d927f36233
yingchi1130 <kiki001130@gmail.com>
2023-11-21 11:44:14 +0800

    week11

---
+++ week11/week11-5a.py
@@ -0,0 +1,4 @@
+#SOIT107_Base_007
+a = input()
+
+print( a[0] , a[1], a[2], a[3], a[4] , sep='   ' , end='')
---
+++ week11/week11-5b.py
@@ -0,0 +1,7 @@
+#SOIT107_Base_009
+a = int(input())
+
+if a-2 >0:
+	print( a-2, end = '' )
+else:
+	print( 2-a , end = '')
