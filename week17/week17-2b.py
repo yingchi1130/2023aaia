200a5296b94d8eb60531aac6d30093c3e15647ab
yingchi1130 <kiki001130@gmail.com>
2024-01-02 10:24:00 +0800

    week17

---
+++ week17/week17-2b.py
@@ -0,0 +1,12 @@
+#SOIT107_ADVANCE_012
+a = int(input())
+
+for i in range(a):
+	space = a-1 - i
+	for k in range(space):
+		print(' ',end='')
+		
+	star = i*2 + 1
+	for k in range(star):
+		print('*',end='')
+	print()
