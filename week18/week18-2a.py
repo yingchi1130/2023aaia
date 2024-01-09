259629b0fceba335fa5d9eb02df5325c25f46a1e
yingchi1130 <kiki001130@gmail.com>
2024-01-09 10:38:18 +0800

    week18

---
+++ week18/week18-2a.py
@@ -0,0 +1,6 @@
+#SOIT106_BASE_011
+a,b = list(map(int,input().split()))
+if a<b: ans = -1
+if a==b: ans = 0
+if a>b: ans = 1
+print(ans)
---
+++ week18/week18-2b.py
@@ -0,0 +1,7 @@
+#SOIT106_BASE_012
+a= int(input())
+if a>=90: ans = 'A'
+elif 80<=a<90: ans = 'B'
+elif 60<=a<80: ans = 'C'
+else: ans = 'F'
+print(ans)
